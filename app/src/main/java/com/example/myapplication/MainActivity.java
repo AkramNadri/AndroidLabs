c4d826d21f68a1f26aad750aaa4550e19fc0a733
Akram <akram.nadri@gmail.com>
2019-01-28 16:13:53 -0500

    lab 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lab3);
+
+
+        emailInput = findViewById(R.id.line2);
+
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String sharedEmail = sharedPref.getString(getString(R.string.sharedEmail), "");
+
+        emailInput.setHint(sharedEmail);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.sharedEmail), emailInput.getText().toString());
+        editor.commit();
     }
 
 
