5dd2dd458aad7876f8c9a109bdb86daa2dc44a0b
Akram <akram.nadri@gmail.com>
2019-01-29 16:26:54 -0500

    lab 3 - 2nd commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lab3_MainActivity);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
 
         emailInput = findViewById(R.id.line2);
 
 
-        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         String sharedEmail = sharedPref.getString(getString(R.string.sharedEmail), "");
 
         emailInput.setHint(sharedEmail);
@@ -36,27 +36,5 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
     }
 
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageButton.setImageBitmap(imageBitmap);
-        }
-    }
-
-
-
 }
+
---
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    ImageButton mImageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mImageButton = findViewById(R.id.image_capture_button);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public static final String ACTIVITY_NAME = "ProfileActivity";
+    Log.e(ACTIVITY_NAME, "In function:" + onCreate);
+
+    //You will have to create a static String variable called ACTIVITY_NAME,
+    // and set it equal to the Activity’s name. In this case:
+
+
+}
