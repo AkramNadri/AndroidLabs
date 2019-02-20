b82027286161d692f9061cdd26ac07a5d90e4e3d
Akram <akram.nadri@gmail.com>
2019-02-20 16:23:15 -0500

    lab 5 - 2nd commit

--- app/src/main/java/com/example/myapplication/ChatRoomActivity.java
+++ app/src/main/java/com/example/myapplication/ChatRoomActivity.java
@@ -1,17 +1,17 @@
 package com.example.myapplication;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
-
 import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
@@ -20,29 +20,6 @@ public class ChatRoomActivity extends AppCompatActivity {
     ListView theList;
     ArrayList<Message> messages;
     MyOwnAdapter adapter;
-
-    // query goes in here
-    public void sendMessage(View v) {
-        String text = chatMess.getText().toString();
-        if (!text.equals("")) {
-            Message m = new Message(text, true);
-            messages.add(m);
-            theList.setAdapter(adapter);
-            chatMess.setText("");
-        }
-    }
-
-    // query goes in here
-    public void receiveMessaage(View v) {
-        String text = chatMess.getText().toString();
-        if (!text.equals("")) {
-            Message m = new Message(text, false);
-
-            messages.add(m);
-            theList.setAdapter(adapter);
-            chatMess.setText("");
-        }
-    }
 
     // query goes in here
     @Override
@@ -60,7 +37,47 @@ public class ChatRoomActivity extends AppCompatActivity {
 
         theList.setAdapter(adapter);
 
+
+
+        //Get the fields from the screen:
+        EditText chatMessage = (EditText)findViewById(R.id.chatMess);
+        Button receiveMessage = (Button)findViewById(R.id.receive);
+        Button sendMessage = (Button)findViewById(R.id.send);
+        ListView theList = (ListView)findViewById(R.id.the_list);
+
+
+
+        //get a database:
+        MyDatabaseOpenHelper dbOpener = new MyDatabaseOpenHelper(this);
+        SQLiteDatabase db = dbOpener.getWritableDatabase();
+
     }
+
+
+    // query goes in here
+    public void sendMessage(View v) {
+        String text = chatMess.getText().toString();
+        if (!text.equals("")) {
+            Message m = new Message(text, true);
+            messages.add(m);
+            theList.setAdapter(adapter);
+            chatMess.setText("");
+        }
+    }
+
+    // query goes in here
+    public void receiveMessage(View v) {
+        String text = chatMess.getText().toString();
+        if (!text.equals("")) {
+            Message m = new Message(text, false);
+
+            messages.add(m);
+            theList.setAdapter(adapter);
+            chatMess.setText("");
+        }
+    }
+
+
 
     protected class MyOwnAdapter extends BaseAdapter {
         private ArrayList<Message> messages;
